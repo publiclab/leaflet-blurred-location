f45c4cf303f829e6fc500331aecf282cd494370d
Mridul Nagpal <mridulnagpal07@gmail.com>
2017-05-16 22:27:29 +0530

    tweaks

--- spec/javascripts/test_spec.js
+++ spec/javascripts/test_spec.js
@@ -1,14 +1,14 @@
 /*global BlurredLocation */
 describe("Basic suite", function() {
   "use strict";
-  var object = new BlurredLocation({
+  var blurredLocatoion = new BlurredLocation({
     lat: 41.01,
     lon: -85.66
   });
   it("lat spec", function() {
-    expect(object.getLat()).toBe(41.01);
+    expect(blurredLocatoion.getLat()).toBe(41.01);
     });
   it("lon spec", function() {
-    expect(object.getLon()).toBe(-85.66);
+    expect(blurredLocatoion.getLon()).toBe(-85.66);
   });
 });
