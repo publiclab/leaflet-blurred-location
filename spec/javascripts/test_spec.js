fc30639abe13428a88f420478a8f1da35e3a850b
Mridul Nagpal <mridulnagpal07@gmail.com>
2017-05-22 02:14:31 +0530

    geocode and tests

--- spec/javascripts/test_spec.js
+++ spec/javascripts/test_spec.js
@@ -1,21 +1,38 @@
 var map;
 
-describe("Testing changing view", function() {
+describe("Basic testing", function() {
   "use strict";
 
   var fixture = loadFixtures('index.html');
   map = L.map('map');
 
-  var blurredLocatoion = new BlurredLocation({
+  var blurredLocation = new BlurredLocation({
     lat: 41.01,
     lon: -85.66
   });
 
-  it("main spec", function() {
-    expect(blurredLocatoion.getLat()).toBe(41.01);
-    expect(blurredLocatoion.getLon()).toBe(-85.66);
-    map.setView([51.50, -0.09], 13);
-    expect(blurredLocatoion.getLat()).toBe(51.50);
-    expect(blurredLocatoion.getLon()).toBe(-0.09);
+  it("getLat spec", function () {
+    expect(blurredLocation.getLat()).toBe(41.01);
   });
+
+  it("getLon spec", function () {
+    expect(blurredLocation.getLon()).toBe(-85.66);
+  });
+
+  it("goTo spec", function() {
+    expect(blurredLocation.getLat()).toBe(41.01);
+    expect(blurredLocation.getLon()).toBe(-85.66);
+    blurredLocation.goTo(51.50, -0.09, 13);
+    expect(blurredLocation.getLat()).toBe(51.50);
+    expect(blurredLocation.getLon()).toBe(-0.09);
+  });
+
+  it("geocode spec", function() {
+    var geometry = blurredLocation.geocode("Buenos Aires");
+    console.log(blurredLocation.getLat());
+    console.log(map.getCenter().lat);
+    expect(blurredLocation.getLat()).toBe(-34.6036844);
+    expect(blurredLocation.getLon()).toBe(-58.3815591);
+  });
+
 });
--- src/object.js
+++ src/object.js
@@ -7,13 +7,21 @@ function BlurredLocation(location) {
       this.getLon = function() {
         return map.getCenter().lng;
       }
-    }
-    else {
-      this.getLat = function () {
-        return location['lat'];
+      this.goTo = function(lat, lon, zoom) {
+        map.setView([lat, lon], zoom);
       }
-      this.getLon = function() {
-        return location['lon'];
+      this.geocode = function(string) {
+        $.ajaxSetup({async: false});
+        var url = "https://maps.googleapis.com/maps/api/geocode/json?address="+string.split(" ").join("+");
+        var temp = $.ajax({
+            async: false,
+            url: url
+        });
+        var geometry = temp.responseJSON.results[0].geometry.location;
+        var default_zoom = 13;
+        console.log(geometry.lat);
+        map.setView([geometry.lat, geometry.lng],default_zoom);
+        return geometry;
       }
     }
 }
