81efadc72fb6e00a24db3fd3cc2d04cd9114afc1
Mridul Nagpal <mridulnagpal07@gmail.com>
2017-06-15 20:49:23 +0530

    Structuring the library

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function(grunt) {
 
         browserify: {
             dist: {
-                src: ['node_modules/jquery/dist/jquery.min.js', 'node_modules/leaflet/dist/leaflet.js', 'src/object.js'],
+                src: ['node_modules/jquery/dist/jquery.min.js', 'node_modules/leaflet/dist/leaflet.js', 'src/core/object.js'],
                 dest: 'dist/built.js'
             }
         },
