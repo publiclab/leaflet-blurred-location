16ae435c0032cfb50e00b6788edaf93c2430d06c
Mridul Nagpal <mridulnagpal07@gmail.com>
2017-06-17 17:54:10 +0530

    Update object.js

--- src/core/object.js
+++ src/core/object.js
@@ -48,17 +48,17 @@ BlurredLocation = function BlurredLocation(options) {
 
   addGrid = options.addGrid;
 
-  // function panMapToGeocodedLocation(selector) {
-  //   var input = document.getElementById(selector);
-  //
-  //   var autocomplete = new google.maps.places.Autocomplete(input);
-  //   autocomplete.addListener('place_changed', function() {
-  //     setTimeout(function () {
-  //       var str = input.value;
-  //       blurredLocation.geocode(str);
-  //     }, 10);
-  //   });
-  // };
+  function panMapToGeocodedLocation(selector) {
+    var input = document.getElementById(selector);
+  
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.addListener('place_changed', function() {
+      setTimeout(function () {
+        var str = input.value;
+        blurredLocation.geocode(str);
+      }, 10);
+    });
+  };
 
   function panMapWhenInputsChange(latId, lngId) {
 
@@ -118,7 +118,7 @@ BlurredLocation = function BlurredLocation(options) {
     geocode: geocode,
     getSize: getSize,
     addGrid: addGrid,
-    // panMapToGeocodedLocation: panMapToGeocodedLocation,
+    panMapToGeocodedLocation: panMapToGeocodedLocation,
     getPlacenameFromCoordinates: getPlacenameFromCoordinates,
     panMapWhenInputsChange: panMapWhenInputsChange,
     panMap: panMap,
