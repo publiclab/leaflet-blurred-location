f3c3784508efa8be3e5e9e0205c2305f65c928c9
Mridul Nagpal <mridulnagpal07@gmail.com>
2017-06-17 18:14:01 +0530

    tweaks

--- src/blurredLocation.js
+++ src/blurredLocation.js
@@ -1,44 +1,52 @@
 BlurredLocation = function BlurredLocation(options) {
-    options = options || {};
-    options.map = options.map || L.map('map');
-    options.addGrid = options.addGrid || require('./core/addGrid.js');
 
-    L.tileLayer("https://a.tiles.mapbox.com/v3/jywarren.map-lmrwb2em/{z}/{x}/{y}.png").addTo(options.map);
-    options.location = options.location || {
-      lat: 41.01,
-      lon: -85.66
-    };
-    options.zoom = options.zoom || 13;
-    options.map.setView([options.location.lat, options.location.lon], options.zoom);
+  var L = require('leaflet');
+  var blurredLocation = this;
 
-    function getLat() {
-      return options.map.getCenter().lat;
-    }
+  options = options || {};
+  options.map = options.map || L.map('map');
 
-    function getLon() {
-      return options.map.getCenter().lng;
-    }
+  options.addGrid = options.addGrid || require('./core/addGrid.js');
+  options.addGrid(options.map);
 
-    function goTo(lat, lon, zoom) {
-      options.map.setView([lat, lon], zoom);
-    }
+  L.tileLayer("https://a.tiles.mapbox.com/v3/jywarren.map-lmrwb2em/{z}/{x}/{y}.png").addTo(options.map);
 
-    function geocode(string) {
-      var url = "https://maps.googleapis.com/maps/api/geocode/json?address="+string.split(" ").join("+");
-      var Blurred = $.ajax({
-          async: false,
-          url: url
-      });
-      var geometry = Blurred.responseJSON.results[0].geometry.location;
-      options.map.setView([geometry.lat, geometry.lng],options.zoom);
-      return geometry;
-    }
+  options.location = options.location || {
+    lat: 41.01,
+    lon: -85.66
+  };
 
-    function getSize() {
-      return options.map.getSize();
-    }
+  options.zoom = options.zoom || 13;
+  options.map.setView([options.location.lat, options.location.lon], options.zoom);
 
-    addGrid = options.addGrid;
+  function getLat() {
+    return options.map.getCenter().lat;
+  }
+
+  function getLon() {
+    return options.map.getCenter().lng;
+  }
+
+  function goTo(lat, lon, zoom) {
+    options.map.setView([lat, lon], zoom);
+  }
+
+  function geocode(string) {
+    var url = "https://maps.googleapis.com/maps/api/geocode/json?address="+string.split(" ").join("+");
+    var Blurred = $.ajax({
+        async: false,
+        url: url
+    });
+    var geometry = Blurred.responseJSON.results[0].geometry.location;
+    options.map.setView([geometry.lat, geometry.lng],options.zoom);
+    return geometry;
+  }
+
+  function getSize() {
+    return options.map.getSize();
+  }
+
+  addGrid = options.addGrid;
 
   function panMapToGeocodedLocation(selector) {
     var input = document.getElementById(selector);
@@ -47,13 +55,12 @@ BlurredLocation = function BlurredLocation(options) {
     autocomplete.addListener('place_changed', function() {
       setTimeout(function () {
         var str = input.value;
-        blurredLocation.geocode(str);
+        geocode(str);
       }, 10);
     });
   };
 
   function panMapWhenInputsChange(latId, lngId) {
-
     var lat = document.getElementById(latId);
     var lng = document.getElementById(lngId);
 
@@ -61,20 +68,19 @@ BlurredLocation = function BlurredLocation(options) {
       if(lat.value && lng.value) {
         panMap(lat.value, lng.value);
       };
-    };
+    }
 
-    lat.addEventListener('change', function() {
+    lat.addEventListener('change blur input', function() {
       panIfValue();
     });
-
-    lng.addEventListener('change', function() {
+    lng.addEventListener('change blur input', function() {
       panIfValue();
     });
   }
 
 
   function panMap(lat, lng) {
-    options.map.panTo(new L.LatLng(lat, lng));
+    map.panTo(new L.LatLng(lat, lng));
   }
 
   function getPlacenameFromCoordinates(lat, lng) {
@@ -92,9 +98,9 @@ BlurredLocation = function BlurredLocation(options) {
     var x = document.getElementById("location");
       if(checkbox.checked == true) {
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(displayPosition);
+          navigator.geolocation.getCurrentPosition(displayPosition);
         } else {
-            x.innerHTML = "Geolocation is not supported by this browser.";
+          x.innerHTML = "Geolocation is not supported by this browser.";
         }
 
         function displayPosition(position) {
