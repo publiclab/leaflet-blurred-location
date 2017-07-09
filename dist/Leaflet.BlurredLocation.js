9f91e981a2c684e8e82e6214b761ed41c9357925
Mridul Nagpal <mridulnagpal07@gmail.com>
2017-07-10 00:56:32 +0530

    tweaks

--- dist/Leaflet.BlurredLocation.js
+++ dist/Leaflet.BlurredLocation.js
@@ -14333,10 +14333,8 @@ module.exports = function gridSystem(options) {
   options.graticuleOptions = options.graticuleOptions || {
                  showLabel: true,
                  zoomInterval: [
-                    {start: 2, end: 2, interval: 40},
-                    {start: 3, end: 3, interval: 20},
-                    {start: 4, end: 4, interval: 10},
-                    {start: 5, end: 6, interval: 5},
+                    {start: 2, end: 2, interval: 100},
+                    {start: 3, end: 5, interval: 10},
                     {start: 6, end: 8, interval: 1},
                     {start: 9, end: 12, interval: 0.1},
                     {start: 13, end: 15, interval: 0.01},
