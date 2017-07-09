17cbe34176c37c963ddd07de41c29e2376665a30
Mridul Nagpal <mridulnagpal07@gmail.com>
2017-07-10 00:05:50 +0530

    Intervals set according to precision

--- src/core/gridSystem.js
+++ src/core/gridSystem.js
@@ -11,8 +11,12 @@ module.exports = function gridSystem(options) {
                     {start: 2, end: 2, interval: 40},
                     {start: 3, end: 3, interval: 20},
                     {start: 4, end: 4, interval: 10},
-                    {start: 5, end: 7, interval: 5},
-                    {start: 8, end: 20, interval: 1}
+                    {start: 5, end: 6, interval: 5},
+                    {start: 6, end: 8, interval: 1},
+                    {start: 9, end: 12, interval: 0.1},
+                    {start: 13, end: 15, interval: 0.01},
+                    {start: 16, end: 20, interval: 0.001},
+
                   ],
                  opacity: 1,
                  color: '#ff0000',
