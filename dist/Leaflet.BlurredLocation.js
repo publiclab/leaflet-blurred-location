3a8a38598e42a82e5b35c863edb942e2ea6a35f4
Mridul Nagpal <mridulnagpal07@gmail.com>
2017-08-13 01:53:03 +0530

    geocodeWithBrowser added

--- dist/Leaflet.BlurredLocation.js
+++ dist/Leaflet.BlurredLocation.js
@@ -782,6 +782,14 @@ BlurredLocation = function BlurredLocation(options) {
 
   enableCenterShade();
 
+  function geocodeWithBrowser(boolean) {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+      goTo(position.coords.latitude, position.coords.longitude,options.zoom);
+      });
+    }
+  }
+
   return {
     getLat: getLat,
     getLon: getLon,
@@ -808,6 +816,7 @@ BlurredLocation = function BlurredLocation(options) {
     disableCenterShade: disableCenterShade,
     enableCenterShade: enableCenterShade,
     geocodeStringAndPan: geocodeStringAndPan,
+    geocodeWithBrowser: geocodeWithBrowser,
   }
 }
 
--- src/blurredLocation.js
+++ src/blurredLocation.js
@@ -249,6 +249,14 @@ BlurredLocation = function BlurredLocation(options) {
 
   enableCenterShade();
 
+  function geocodeWithBrowser(boolean) {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+      goTo(position.coords.latitude, position.coords.longitude,options.zoom);
+      });
+    }
+  }
+
   return {
     getLat: getLat,
     getLon: getLon,
@@ -275,6 +283,7 @@ BlurredLocation = function BlurredLocation(options) {
     disableCenterShade: disableCenterShade,
     enableCenterShade: enableCenterShade,
     geocodeStringAndPan: geocodeStringAndPan,
+    geocodeWithBrowser: geocodeWithBrowser,
   }
 }
 
