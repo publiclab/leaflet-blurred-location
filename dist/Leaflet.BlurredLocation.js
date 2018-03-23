ac668b4dae3da7f66b4ff31955f9a3174730b2cb
mridulnagpal <mridulnagpal07@gmail.com>
2018-03-23 16:04:45 +0530

    2 more methods separated

--- dist/Leaflet.BlurredLocation.js
+++ dist/Leaflet.BlurredLocation.js
@@ -612,71 +612,8 @@ BlurredLocation = function BlurredLocation(options) {
     return options.map.getSize();
   }
 
-  function panMapToGeocodedLocation(selector) {
-    var input = document.getElementById(selector);
-
-    var autocomplete = new google.maps.places.Autocomplete(input);
-    autocomplete.addListener('place_changed', function() {
-      setTimeout(function () {
-        var str = input.value;
-        Geocoding.geocodeStringAndPan(str);
-      }, 10);
-    });
-  };
-
   function panMap(lat, lng) {
     options.map.panTo(new L.LatLng(lat, lng));
-  }
-
-  // function getPlacenameFromCoordinates(lat, lng, precision, onResponse) {
-  //     $.ajax({
-  //       url:"https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lng,
-  //       success: function(result) {
-  //         if(result.results[0]) {
-  //           var country;
-  //           var fullAddress = result.results[0].formatted_address.split(",");
-  //           for (i in result.results) {
-  //             if(result.results[i].types.indexOf("country") != -1) {
-  //               //If the type of location is a country assign it to thr input box value
-  //               country = result.results[i].formatted_address;
-  //             }
-  //           }
-  //           if (!country) country = fullAddress[fullAddress.length - 1];
-
-  //           if(precision <= 0) onResponse(country);
-
-  //           else if(precision == 1) {
-  //             if (fullAddress.length>=2) onResponse(fullAddress[fullAddress.length - 2] + ", " + country);
-  //             else onResponse(country);
-  //           }
-
-  //           else if(precision >= 2) {
-  //             if (fullAddress.length >= 3) onResponse(fullAddress[fullAddress.length - 3] + ", " + fullAddress[fullAddress.length - 2] + ", " + country);
-  //             else if (fullAddress.length == 2) onResponse(fullAddress[fullAddress.length - 2] + ", " + country);
-  //             else onResponse(country);
-  //           }
-
-  //           else onResponse(result.results[0].formatted_address);
-
-  //       }
-  //       else onResponse("Location unavailable");
-  //     }
-  //   });
-  // }
-
-  function panMapByBrowserGeocode(checkbox) {
-    var x = document.getElementById("location");
-      if(checkbox.checked == true) {
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(displayPosition);
-        } else {
-          x.innerHTML = "Geolocation is not supported by this browser.";
-        }
-
-        function displayPosition(position) {
-          panMap(parseFloat(position.coords.latitude), parseFloat(position.coords.longitude));
-        }
-    }
   }
 
   function gridWidthInPixels(degrees) {
@@ -806,13 +743,13 @@ BlurredLocation = function BlurredLocation(options) {
 
     updateRectangleOnPan();
 
-  function geocodeWithBrowser(boolean) {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-      goTo(position.coords.latitude, position.coords.longitude,options.zoom);
-      });
-    }
-  }
+  // function geocodeWithBrowser(boolean) {
+  //   if ("geolocation" in navigator) {
+  //     navigator.geolocation.getCurrentPosition(function(position) {
+  //     goTo(position.coords.latitude, position.coords.longitude,options.zoom);
+  //     });
+  //   }
+  // }
 
   function displayLocation() {
     var lat = getLat();
@@ -826,10 +763,10 @@ BlurredLocation = function BlurredLocation(options) {
     goTo: goTo,
     getSize: getSize,
     gridSystem: gridSystem,
-    panMapToGeocodedLocation: panMapToGeocodedLocation,
+    panMapToGeocodedLocation: Geocoding.panMapToGeocodedLocation,
     getPlacenameFromCoordinates: Geocoding.getPlacenameFromCoordinates,
     panMap: panMap,
-    panMapByBrowserGeocode: panMapByBrowserGeocode,
+    panMapByBrowserGeocode: Geocoding.panMapByBrowserGeocode,
     getMinimumGridWidth: getMinimumGridWidth,
     gridWidthInPixels: gridWidthInPixels,
     getPrecision: getPrecision,
@@ -846,7 +783,7 @@ BlurredLocation = function BlurredLocation(options) {
     disableCenterShade: disableCenterShade,
     enableCenterShade: enableCenterShade,
     geocodeStringAndPan: Geocoding.geocodeStringAndPan,
-    geocodeWithBrowser: geocodeWithBrowser,
+    geocodeWithBrowser: Geocoding.geocodeWithBrowser,
     displayLocation: displayLocation,
   }
 }
@@ -909,10 +846,47 @@ module.exports = function Geocoding(options) {
     });
   }
 
+  function panMapByBrowserGeocode(checkbox) {
+    var x = document.getElementById("location");
+      if(checkbox.checked == true) {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(displayPosition);
+        } else {
+          x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+
+        function displayPosition(position) {
+          panMap(parseFloat(position.coords.latitude), parseFloat(position.coords.longitude));
+        }
+    }
+  }
+
+  function panMapToGeocodedLocation(selector) {
+    var input = document.getElementById(selector);
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.addListener('place_changed', function() {
+      setTimeout(function () {
+        var str = input.value;
+        Geocoding.geocodeStringAndPan(str);
+      }, 10);
+    });
+  };
+
+  function geocodeWithBrowser(boolean) {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+      goTo(position.coords.latitude, position.coords.longitude,options.zoom);
+      });
+    }
+  }
 
   return {
     geocodeStringAndPan: geocodeStringAndPan,
-    getPlacenameFromCoordinates: getPlacenameFromCoordinates
+    getPlacenameFromCoordinates: getPlacenameFromCoordinates,
+    panMapByBrowserGeocode: panMapByBrowserGeocode,
+    panMapToGeocodedLocation: panMapToGeocodedLocation,
+    geocodeWithBrowser: geocodeWithBrowser
   }
 }
 
